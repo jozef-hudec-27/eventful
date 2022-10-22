b86737a35f21e54b101fd9c645a24a84349f1426
Jozef Hudec <jojohudec27@gmail.com>
2022-10-22 11:04:19 +0200

    Display message when no events are found

--- app/javascript/custom/search_event.js
+++ app/javascript/custom/search_event.js
@@ -3,3 +3,5 @@ document.addEventListener('keydown', e => {
       window.location.href = '/events/?q=' + document.getElementById('search-event-input').value;
   }
 });
+
+document.getElementById('search-event-input').value = new URLSearchParams(window.location.search).get('q')
